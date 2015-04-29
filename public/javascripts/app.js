b909a1dc8bf0bfc83f53930f87b86990e74063b7
Annapurna Singh <annapurna.singh@gmail.com>
2015-04-29 15:41:00 -0500

    JS_Styles_Views

---
+++ public/javascripts/app.js
@@ -0,0 +1,47 @@
+var app = app || {};
+
+$(document).ready(function(){
+
+  app.foods = new app.FoodCollection({
+    model: app.FoodModel
+  })
+
+  app.parties = new app.PartyCollection({
+    model: app.PartyModel
+  })
+
+  app.foodListPainter = new app.GeneralListView({
+    modelView: app.FoodView,
+    collection: app.foods,
+    el: $('#menu-list'),
+  });
+
+  app.partyListPainter = new app.GeneralListView({
+    modelView: app.PartyView,
+    collection: app.parties,
+    el: $('#party-list'),
+  });
+
+  app.parties.fetch();
+  app.foods.fetch();
+
+  $('#place-order').on('click', function(){
+
+    var partyId = app.partySelection.get('id');
+    var foodId = app.foodSelection.get('id');
+
+    $.ajax({
+      method: 'post',
+      url: '/api/orders',
+      data: {order: {party_id: partyId, food_id: foodId} },
+      success: function(){
+        app.parties.fetch( {reset: true} );
+
+        $('.food-selected').removeClass('food-selected');
+        $('.party-selected').removeClass('party-selected');
+      }
+    });
+
+  });
+
+});
---
+++ public/javascripts/models/FoodModel.js
@@ -0,0 +1,2 @@
+var app = app || {};
+app.FoodModel = Backbone.Model.extend({});
